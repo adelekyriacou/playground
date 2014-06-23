e50288c874263a4692d4a5fe2ab9b0cc15f33b2e
Adele Kyriacou <adelekyriacou@gmail.com>
2014-06-23 15:04:47 +0100

    Initialise pangram.js & the spec

---
+++ Pangram/pangram.js
@@ -0,0 +1,21 @@
+function isPangram(sentence) {
+  var sentenceLower = sentence.toLowerCase(); // makes all characters lowercase
+  var alpha = 'abcdefghijklmnopqrstuvwxyz'; // assigns string to variable
+  var alphaArray = alpha.split(''); // splits string into array & assigns to variable
+  var hasEveryLetter = true; // assigns true to variable
+ 
+  alphaArray.forEach(function (letter) { // for each letter in array pass letter into function
+    if (stringDoesntContainCharacter(sentenceLower, letter)) { // if 
+      hasEveryLetter = false;
+    }
+  });
+ 
+  return hasEveryLetter;
+}
+ 
+function stringDoesntContainCharacter(str, c) { // two parameters, string & characater
+  return str.indexOf(c) === -1; // returns true when c isn't in string
+}
+ 
+//str.indexOf(c) = tells where the character first appears in the string, index form.
+//                 returns -1 if it doesn't appear in string at all.
